292cdb0453c6591ed6efb207bebd3baf63554a77
Mirko Kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-03-18 09:28:52 +0100

    snapshot

--- dune/fem/operator/assembled/acmodel.hh
+++ dune/fem/operator/assembled/acmodel.hh
@@ -12,71 +12,86 @@ template<class Grid, class Problem>
 class HeatModel
 {
   public:
-  typedef Problem ProblemType;
-  typedef Grid GridType;
-  enum{ dimDomain = GridType::dimensionworld };
- 
-  enum{ dimRange = ProblemType::dimRange };
-  
-  typedef typename ProblemType::ThermodynamicsType ThermodynamicsType;
-  typedef double RangeFieldType; 
-  typedef typename Dune::FieldVector<RangeFieldType,dimRange> RangeType;
-  typedef typename Dune::FieldVector<RangeFieldType,dimDomain> DomainType; 
-  typedef typename Dune::FieldMatrix<RangeFieldType,dimRange,dimDomain> JacobianRangeType;
+    typedef Problem ProblemType;
+    typedef Grid GridType;
+    enum{ dimDomain = GridType::dimensionworld };
 
-  typedef PhasefieldFilter<RangeType> Filter;
+    enum{ dimRange = ProblemType::dimRange };
+
+    typedef typename ProblemType::ThermodynamicsType ThermodynamicsType;
+    typedef double RangeFieldType; 
+    typedef typename Dune::FieldVector<RangeFieldType,dimRange> RangeType;
+    typedef typename Dune::FieldVector<RangeFieldType,dimDomain> DomainType; 
+    typedef typename Dune::FieldMatrix<RangeFieldType,dimRange,dimDomain> JacobianRangeType;
+
+    typedef PhasefieldFilter<RangeType> Filter;
 
 
-//contructor
- public:
- HeatModel( const ProblemType& problem):
-   problem_(problem),
-   penalty_(Dune::Fem::Parameter::getValue<double>("phasefield.penalty"))
+    //contructor
+  public:
+    HeatModel( const ProblemType& problem):
+      problem_(problem),
+      penalty_(Dune::Fem::Parameter::getValue<double>("phasefield.penalty"))
   {}
 
 
-  inline void totalEnergy( const DomainType& xgl,
-                           RangeType& vu,
-                           double& kin,
-                           double& therm,
-                           double& total) const;
-                       
-  // additional Source for the whole syten eg. for 
-  // generatring exact solutions
-  inline void systemSource( const double time,
-                            const DomainType& xgl,
-                            RangeType& s) const;
+    inline void totalEnergy( const DomainType& xgl,
+        RangeType& vu,
+        double& kin,
+        double& therm,
+        double& total) const;
+
+    // additional Source for the whole syten eg. for 
+    // generatring exact solutions
+    inline void systemSource( const double time,
+        const DomainType& xgl,
+        RangeType& s) const;
 
 
 
-  inline void  muSource( const RangeFieldType rho1,
-                         const RangeFieldType rho2,
-                         const RangeFieldType phi,
-                         RangeFieldType& mu) const;
-  
-  inline void tauSource( const RangeFieldType phi1,
-                         const RangeFieldType phi2,
-                         const RangeFieldType rho,
-                         RangeFieldType& tau) const;
- 
-  inline void dirichletValue( const double time,
-                              const DomainType& xglobal,
-                              RangeType& g) const;
-  
-  inline  double penalty() const
+    inline void  muSource( const RangeFieldType rho1,
+        const RangeFieldType rho2,
+        const RangeFieldType phi,
+        RangeFieldType& mu) const;
+
+    inline void  drhomuSource( const RangeFieldType rho1,
+        const RangeFieldType rho2,
+        const RangeFieldType phi,
+        RangeFieldType& mu) const;
+
+    inline void  dphimuSource( const RangeFieldType rho1,
+        const RangeFieldType rho2,
+        const RangeFieldType phi,
+        RangeFieldType& mu) const;
+
+    inline void tauSource( const RangeFieldType phi1,
+        const RangeFieldType phi2,
+        const RangeFieldType rho,
+        RangeFieldType& tau) const;
+
+    inline void dphitauSource( const RangeFieldType phi1,
+        const RangeFieldType phi2,
+        const RangeFieldType rho,
+        RangeFieldType& tau) const;
+
+    inline void dirichletValue( const double time,
+        const DomainType& xglobal,
+        RangeType& g) const;
+
+    inline  double penalty() const
     {
       return penalty_;
     }
-  
-  inline void diffusion(JacobianRangeType& vu,
-                        JacobianRangeType& diffusion) const;
+
+    inline void diffusion(JacobianRangeType& vu,
+        JacobianRangeType& diffusion) const;
 
 
-  inline double delta() const
+    inline double delta() const
     {
       return problem_.thermodynamics().delta();
     }
-  inline double deltaInv() const
+    inline double deltaInv() const
     {
       return problem_.thermodynamics().deltaInv();
     }
@@ -91,55 +106,81 @@ class HeatModel
 template< class Grid, class Problem>
 inline void HeatModel< Grid,Problem>
 ::totalEnergy(const DomainType& xgl,
-              RangeType& vu,
-              double& kin,
-              double& therm,
-              double& total) const
+    RangeType& vu,
+    double& kin,
+    double& therm,
+    double& total) const
+{
+  double rho=Filter::rho(vu);
+  double phi=Filter::phi(vu);
+  double kineticEnergy{0.},surfaceEnergy{0.};
+  for(int i=0; i<dimDomain; i++)
   {
-    double rho=Filter::rho(vu);
-    double phi=Filter::phi(vu);
-    double kineticEnergy{0.},surfaceEnergy{0.};
-    for(int i=0; i<dimDomain; i++)
-    {
-      kineticEnergy+=Filter::velocity(vu,i)*Filter::velocity(vu,i);
+    kineticEnergy+=Filter::velocity(vu,i)*Filter::velocity(vu,i);
 #if DGSCHEME
-      surfaceEnergy+=Filter::sigma(vu,i)*Filter::sigma(vu,i);
+    surfaceEnergy+=Filter::sigma(vu,i)*Filter::sigma(vu,i);
 #elif FEMSCHEME
 #warning "TOTAL ENERGY NEEDS JACOBIANRANGETYPE - NOT IMPLEMENTED"
 #endif
-    }
-  
-    kin=rho*0.5*kineticEnergy;
-    surfaceEnergy*=0.5;
-    surfaceEnergy*=problem_.thermodynamics().delta();
-  
-    therm=problem_.thermodynamics().helmholtz(rho,phi);
-    therm+=surfaceEnergy;
-   
-    total=therm+kin;
   }
-  
+#if RHOMODEL
+  surfaceEnergy*=rho;
+#else
+#endif
+  kin=rho*0.5*kineticEnergy;
+  surfaceEnergy*=0.5;
+  surfaceEnergy*=problem_.thermodynamics().delta();
+
+  therm=problem_.thermodynamics().helmholtz(rho,phi);
+  therm+=surfaceEnergy;
+
+  total=therm+kin;
+}
+
 template< class Grid, class Problem>
 inline void HeatModel< Grid,Problem>
 ::systemSource( const double time,
-                const DomainType& xgl,
-                RangeType& s) const
-  {
-    s=0.;
+    const DomainType& xgl,
+    RangeType& s) const
+{
+  s=0.;
+}
 
-
-     
-  }
-  
 template<class Grid, class Problem > 
 inline void HeatModel< Grid, Problem >
 ::muSource( RangeFieldType rho1,
             RangeFieldType rho2,
             RangeFieldType phi,
             RangeFieldType& mu) const
-  {
-    mu=problem_.thermodynamics().chemicalPotential(rho1,phi);
-  }
+{
+
+  mu=problem_.thermodynamics().chemicalPotential(rho1,phi);
+
+}
+template<class Grid, class Problem > 
+inline void HeatModel< Grid, Problem >
+::drhomuSource( RangeFieldType rho1,
+                RangeFieldType rho2,
+                RangeFieldType phi,
+                RangeFieldType& mu) const
+
+{
+  mu=problem_.thermodynamics().drhochemicalPotential(rho1,phi);
+
+}
+template<class Grid, class Problem > 
+inline void HeatModel< Grid, Problem >
+::dphimuSource( RangeFieldType rho1,
+                RangeFieldType rho2,
+                RangeFieldType phi,
+                RangeFieldType& mu) const
+{
+  mu=problem_.thermodynamics().dphichemicalPotential(rho1,phi);
+
+}
+
+
+
 
 template< class Grid, class Problem > 
 inline void HeatModel< Grid, Problem>
@@ -147,10 +188,21 @@ inline void HeatModel< Grid, Problem>
             RangeFieldType phi2,
             RangeFieldType rho,
             RangeFieldType& tau) const
-  {
-    tau=problem_.thermodynamics().reactionSource(rho,phi1);
-   // tau=0;
-  }
+{
+  tau=problem_.thermodynamics().reactionSource(rho,phi1);
+
+}
+template< class Grid, class Problem > 
+inline void HeatModel< Grid, Problem>
+::dphitauSource(RangeFieldType phi1,
+    RangeFieldType phi2,
+    RangeFieldType rho,
+    RangeFieldType& tau) const
+{
+  tau=problem_.thermodynamics().dphireactionSource(rho,phi1);
+
+}
+
 
 template< class Grid, class Problem>
 inline void HeatModel< Grid,Problem>
@@ -162,25 +214,25 @@ inline void HeatModel< Grid,Problem>
 template< class Grid, class Problem > 
 inline void HeatModel< Grid, Problem>
 ::diffusion( JacobianRangeType& dvu,
-            JacobianRangeType& diffusion) const
-  {
-    diffusion=0;
-    double mu1=problem_.thermodynamics().mu1();
- //   double mu2=problem_.thermodynamics().mu2();
-  
-  //  diffusion*=mu1;
-    
+    JacobianRangeType& diffusion) const
+{
+  diffusion=0;
+  double mu1=problem_.thermodynamics().mu1();
+  //   double mu2=problem_.thermodynamics().mu2();
 
-    for(int ii = 0 ; ii < dimDomain ; ++ii )
-     {
-       for(int jj = 0; jj < dimDomain ; ++ jj)
-        Filter::dvelocity(diffusion,ii,jj )=mu1*Filter::dvelocity(dvu,ii,jj);
-   
-  //     for(int j=0; j<dimDomain ; ++j )
+  //  diffusion*=mu1;
+
+
+  for(int ii = 0 ; ii < dimDomain ; ++ii )
+  {
+    for(int jj = 0; jj < dimDomain ; ++ jj)
+      Filter::dvelocity(diffusion,ii,jj )=mu1*Filter::dvelocity(dvu,ii,jj);
+
+    //     for(int j=0; j<dimDomain ; ++j )
     //      {
-      //      Filter::dvelocity(diffusion,i,j)+=mu2*0.5*(Filter::dvelocity(dvu,i,j)+Filter::dvelocity(dvu,j,i));
-        // }
-      }
+    //      Filter::dvelocity(diffusion,i,j)+=mu2*0.5*(Filter::dvelocity(dvu,i,j)+Filter::dvelocity(dvu,j,i));
+    // }
+  }
 
 }
 
