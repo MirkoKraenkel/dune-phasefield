12e80d8241aa1eb806b45b0e2ad02fb1a1ac91d9
Mirko Kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-06-11 14:01:33 +0200

    new skript

---
+++ src/problems/mixedscheme/run2.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+x=1
+MAXORD=$1
+d=0.01
+l=5
+echo $MAXORD
+while [ $x -le $MAXORD ]
+do
+echo $d
+./phasefielddggmres scheme:2d$x  phasefield.delta:$d fem.adaptation.finestLevel:$(($x+$l)) 
+d=`echo "scale=10;$d/2 "  | bc`
+x=$(( $x + 1))
+done
+
+#make clean
+#make POLORDER=1 phasefield_nc ; mv phasefield_nc nc1
+#make clean
+#make POLORDER=0 phasefield_nc ; mv phasefield_nc nc0
+
+
+#make clean
+#make POLORDER=2 phasefield ; mv phasefield con2
+#make clean
+#make POLORDER=1 phasefield ; mv phasefield con1
+#make clean
+#make POLORDER=1 phasefield ; mv phasefield con0
+
