44c5af5df4a65bedf436b7f34aa1f1550845da78
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-07-16 13:13:49 +0200

    clean up

--- dune/fem/operator/assembled/fluxes/jacobianflux_nofilter.hh
+++ dune/fem/operator/assembled/fluxes/jacobianflux_nofilter.hh
@@ -31,28 +31,28 @@ public:
     }
 
 
-  double numericalFlux( const DomainType& normal, 
-                        const double penaltyFactor,
-                        const RangeType& vuEn,
-                        const RangeType& vuN,
-                        const RangeType& vuEnOld,
-                        const RangeType& vuNbOld,
-                        RangeType& phiEn,
-                        RangeType& phiNb,
-                        RangeType& gLeft,
-                        RangeType& gRight) const;
+  double numericalFlux ( const DomainType& normal, 
+                         const double penaltyFactor,
+                         const RangeType& vuEn,
+                         const RangeType& vuN,
+                         const RangeType& vuEnOld,
+                         const RangeType& vuNbOld,
+                         RangeType& phiEn,
+                         RangeType& phiNb,
+                         RangeType& gLeft,
+                         RangeType& gRight) const;
 
 
-  double diffusionFlux( const DomainType& normal,
-                        const double penaltyFactor,
-                        const RangeType& uEn,
-                        const RangeType& uNb,
-                        const JacobianRangeType& duEn,
-                        const JacobianRangeType& duNb,
-                        RangeType& valueLeft,
-                        RangeType& valueRight,
-                        JacobianRangeType& dvalueLeft,
-                        JacobianRangeType& dvalueRight) const;
+  double diffusionFlux ( const DomainType& normal,
+                         const double penaltyFactor,
+                         const RangeType& uEn,
+                         const RangeType& uNb,
+                         const JacobianRangeType& duEn,
+                         const JacobianRangeType& duNb,
+                         RangeType& valueLeft,
+                         RangeType& valueRight,
+                         JacobianRangeType& dvalueLeft,
+                         JacobianRangeType& dvalueRight) const;
   
   double boundaryFlux( const DomainType& normal, 
                        const double penaltyFactor,                
@@ -180,33 +180,23 @@ double JacobianFlux<Model>
       for(int ii = 0; ii < dimDomain ; ++ii )
         {
           //v^+\cdot n^+
-         // vNormalEn+=Filter::velocity(midEn,i)*normal[i];
           vNormalEn+=midEn[ 1 + ii ]*normal[ ii ];
           //v^-\cdot n^+
-          //vNormalNb+=Filter::velocity(midNb,i)*normal[i];
           vNormalNb+=midNb[ 1 + ii ]*normal[ ii ];
           //Phi[v]^+\cdot n^+
-          //testNormalEn+=Filter::velocity(phiEn,i)*normal[i];
           testNormalEn+=phiEn[ 1 + ii ]*normal[ ii ];
           //Phi[v]^-\cdot n^+
-          //testNormalNb+=Filter::velocity(phiNb,i)*normal[i];
           testNormalNb+=phiNb[ 1 + ii ]*normal[ ii ];
         }
   
 
       //F_1=-0.5*( \rho^+*v^+\cdot n^+ -\rho^-*v-\cdot n^+)  
-      //Filter::rho(gLeft)=vNormalEn*Filter::rho(phiEn);
       gLeft[ 0 ]=vNormalEn*phiEn[ 0 ];
-      //Filter::rho(gLeft)+=testNormalEn*Filter::rho(midEn);
       gLeft[ 0 ]+=testNormalEn*midEn[ 0 ];
-      //Filter::rho(gLeft)*=-0.25;
       gLeft[ 0 ]*=-0.25;
       //from linerarization  vuMid=0.5(vu+vuOld), d vuMid/d vu=0.5 
-      //Filter::rho( gRight )=vNormalNb*Filter::rho(phiNb);
       gRight[ 0 ]=vNormalNb*phiNb[ 0 ];
-      //Filter::rho( gRight )+=testNormalNb*Filter::rho(midNb);
       gRight[ 0 ]+=testNormalNb*midNb[ 0 ];
-      //Filter::rho( gRight )*=0.25;
       gRight[ 0 ]*=0.25;
 
       //----------------------------------------------------------------
@@ -217,19 +207,13 @@ double JacobianFlux<Model>
         { 
           //F_2=F_{2.1}+F_{2.2}
           //F_{2.1}=-(\mu^+-\mu^-)*n[i]*\rho^+*0.5;
-          //Filter::velocity(gLeft,i)-=Filter::mu(jump)*normal[i]*Filter::rho(phiEn)*0.5;
           gLeft[ 1 + i ]-=jump[ dimDomain + 2 ]*normal[ i ]*phiEn[ 0 ]*0.5;
-          //Filter::velocity(gLeft,i)-=Filter::mu(phiEn)*normal[i]*Filter::rho(midEn)*0.5;
           gLeft[ 1 + i ]-=phiEn[ dimDomain + 2 ]*normal[ i ]*midEn[ 0 ]*0.5;
-          //Filter::velocity(gRight,i)+=Filter::mu(phiNb)*normal[i]*Filter::rho(midEn)*0.5;
           gRight[ 1 + i ]+=phiNb[ dimDomain + 2 ]*normal[ i ]*midEn[ 0 ]*0.5; 
          
           //F_{2.2}=+(\phi^+-\phi^-)*n[i]*\tau^+*0.5
-          //Filter::velocity(gLeft,i)+= Filter::phi(jump)*normal[i]*Filter::tau(phiEn)*0.5;
           gLeft[ 1 + i ]+=jump[ dimDomain + 1 ]*normal[ i ]*phiEn[ dimDomain + 3 ]*0.5;
-          //Filter::velocity(gLeft,i)+= Filter::phi(phiEn)*normal[i]*Filter::tau(midEn)*0.5;
           gLeft[ 1 + i ]+=phiEn[ dimDomain + 1 ]*normal[ i ]*midEn[ dimDomain + 3 ]*0.5;
-          //Filter::velocity(gRight,i)-= Filter::phi(phiNb)*normal[i]*Filter::tau(midEn)*0.5;
           gRight[ 1 + i ]-=phiNb[ dimDomain + 1 ]*normal[ i ]*midEn[ dimDomain + 3 ]*0.5;
           
           gLeft[ 1 + i ]*=0.5;
@@ -246,27 +230,20 @@ double JacobianFlux<Model>
           //F_{3.1}
        
           //-(\phi^+-\phi^-)*n[i]*v[i]^+*0.5 
-          //Filter::phi(gLeft)-=Filter::phi(phiEn)*normal[i]*Filter::velocity(midEn,i)*0.25;
           gLeft[ dimDomain + 1 ]-=phiEn[ dimDomain + 1 ]*normal[ ii ]*midEn[ 1 + ii ]*0.25;
-          //Filter::phi(gLeft)-=Filter::phi(jump)*normal[i]*Filter::velocity(phiEn,i)*0.25;
           gLeft[ dimDomain + 1 ]-=jump[ dimDomain + 1 ]*normal[ ii ]*phiEn[ 1 + ii ]*0.25;
-          //Filter::phi(gRight)+=Filter::phi(phiNb)*normal[i]*Filter::velocity(midEn,i)*0.25;
           gRight[ dimDomain + 1 ]+=phiNb[ dimDomain + 1 ]*normal[ ii ]*midEn[ 1 + ii ]*0.25;
 
           //tau
           //F_{3.2}
           //(\sigma^+-\sigma^-)\cdot n * 0.5
-          //laplaceFluxLeft+=Filter::sigma(phiEn,i)*normal[i]*0.25;// factor of 0.5 from linearization
           laplaceFluxLeft+=phiEn[ dimDomain + 4 + ii ]*normal[ ii ]*0.25;
-          //laplaceFluxRight-=Filter::sigma(phiNb,i)*normal[i]*0.25;
           laplaceFluxRight-=phiNb[ dimDomain + 4 + ii ]*normal[ ii ]*0.25;
         } 
     
       //----------------------------------------------------------------
       //tau-------------------------------------------------------------
-      //Filter::tau(gLeft)-=model_.delta()*laplaceFluxLeft;
       gLeft[ dimDomain + 3 ]-=model_.delta()*laplaceFluxLeft;
-      //Filter::tau(gRight)-=model_.delta()*laplaceFluxRight;
       gRight[ dimDomain + 3 ]-=model_.delta()*laplaceFluxRight;
       //----------------------------------------------------------------
 
@@ -275,9 +252,7 @@ double JacobianFlux<Model>
         {  
           //F_4
           //(\phi^+-\phi^-)\normal*0.5
-          //Filter::sigma(gLeft,i)=Filter::phi(phiEn)*normal[i]*0.5;
           gLeft[ dimDomain + 4 + ii ]=phiEn[ dimDomain + 1 ]*normal[ ii ]*0.5;
-          //Filter::sigma(gRight,i)=-Filter::phi(phiNb)*normal[i]*0.5;
           gRight[ dimDomain + 4 + ii ]=-phiNb[ dimDomain + 1 ]*normal[ ii ]*0.5;
   
         } 
@@ -354,7 +329,7 @@ double JacobianFlux<Model>
                   JacobianRangeType& dvalue) const
 {
   
-  RangeType jump{0};
+  RangeType jump;
   jump=uEn;
   JacobianRangeType aduEn(0.), aduNb(0.); 
   double integrationElement=normal.two_norm();
