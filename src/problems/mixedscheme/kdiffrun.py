2c1c3e9663062c1ff7b644b13df32c9eb2223428
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-08-25 11:13:16 +0200

    added scripts

--- src/problems/mixedscheme/kdiffrun.py
+++ src/problems/mixedscheme/kdiffrun.py
@@ -2,6 +2,7 @@
 import subprocess,sys
 number=int(sys.argv[1])
 nb=str(number)
-subprocess.call(['./phasefield scheme:test&> pmatrix'+nb],shell=True)
-subprocess.call(['./phasefieldfd scheme:test&> pfd'+nb],shell=True)
-subprocess.call(['kdiff3 pmatrix'+nb+' pfd'+nb],shell=True)
+#subprocess.call(['./phasefield scheme:test&> pmatrix'+nb+'.out'],shell=True)
+subprocess.call(['./phasefieldcoupling scheme:test&> pmatrix'+nb+'.out'],shell=True)
+subprocess.call(['./phasefieldfd scheme:test&> pfd'+nb+'.out'],shell=True)
+subprocess.call(['kdiff3 pmatrix'+nb+'.out pfd'+nb+'.out'],shell=True)
--- src/problems/mixedscheme/makeall.py
+++ src/problems/mixedscheme/makeall.py
@@ -1,11 +1,17 @@
 #! /usr/bin/env python
 import subprocess,sys
-programms=[ 'phasefield', 'phasefieldfd', 'phasefieldfdrho', 'phasefieldfdlambda' , 'phasefieldmfree', 'phasefieldoem']
-begin=int(sys.argv[1])
-end=int(sys.argv[2])
-for p in programms[begin : end ]:
-  outfile=p+'_make.out' 
-  c = subprocess.call(['make'+' '+p+' &>'+outfile], shell=True)
-  print( c )
-  if c == 0:
-    subprocess.call(['rm '+outfile],shell=True)
+programms=[ 'phasefield', 'phasefieldcoupling','phasefieldfd', 'phasefieldfdrho', 'phasefieldfdlambda' , 'phasefieldmfree', 'phasefieldoem']
+#begin=int(sys.argv[1])
+#end=int(sys.argv[2])
+for arg in sys.argv[1:]:
+  progindex=int(arg)
+  if progindex<=5:
+    p=programms[ progindex ]
+#for p in programms[begin : end ]:
+    outfile=p+'_make.out' 
+    c = subprocess.call(['make '+' '+p+' &>'+outfile], shell=True)
+    print( c )
+    if c == 0:
+      subprocess.call(['rm '+outfile],shell=True)
+  else:
+    print('no prog for Index'+str(progindex))
