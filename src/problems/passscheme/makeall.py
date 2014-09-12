2a13c5afe6fd3908744314d7b568a0d77f3907fb
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-09-12 14:35:43 +0200

    choose polorder

--- src/problems/passscheme/makeall.py
+++ src/problems/passscheme/makeall.py
@@ -6,7 +6,7 @@ progdir='prog_P'+polorder
 programms=[ 'phasefield','phasefield_nc', 'phasefield_nctr']
 for p in programms[ : ]:
   outfile=p+'_make.out' 
-  c = subprocess.call(['make POLORDER='+polorder+' ' +p+' &>'+outfile], shell=True)
+  c = subprocess.call(['make CXXFLAGS="-Wfatal-errors" POLORDER='+polorder+' ' +p+' &>'+outfile], shell=True)
   subprocess.call(['mv '+p+' '+p+'_P'+polorder], shell=True)
   print( c )
   if c == 0:
