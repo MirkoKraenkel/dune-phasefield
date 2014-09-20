14ad4da98dc872ef6dd55ece35775b55e1bcfaaa
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-09-20 14:25:41 +0200

    new files

---
+++ src/scripts/pfmaker.py
@@ -0,0 +1,88 @@
+import subprocess,sys,pickle
+from Tkinter import *
+terminal="xfce4-terminal"
+
+#==================================
+#the make class
+#==================================
+class PhasefieldMaker:
+  def __init__(self, master):
+    entryframe=Frame(master)
+    entryframe.pack()
+    listframe=Frame(master)
+    listframe.pack()
+    messageframe=Frame(master)
+    messageframe.pack(side=BOTTOM)
+    self.cxxflags={ 'debug':'-Wfatal-errors -g -Wall' , 'full' :''}
+    self.compiled=[]
+    self.programms=[]
+    progfile=open('progs.txt')
+    for line in progfile:
+      self.programms.append(line.rstrip('\n'))
+    self.programms
+    progfile.close()
+    self.listbox = Listbox( listframe , selectmode=MULTIPLE)
+    self.listbox.pack(side=LEFT)
+    for item in self.programms:
+      self.listbox.insert(END, item)
+    self.var = StringVar() 
+    for item in self.cxxflags:
+      var=IntVar()
+      chck =Radiobutton( entryframe, text=item ,variable=self.var, value=item).pack(side=LEFT)
+    self.make=Button( entryframe, text="Make!", command=self.makeit)
+    self.make.pack(side=RIGHT)
+    self.polord = Entry( entryframe)
+    self.polord.insert(0,'PolOrder')
+    self.polord.pack(side=RIGHT )
+    self.msgvar=StringVar()
+    self.msgvar.set('welcome')
+    self.mymsg = Message( master, textvariable=self.msgvar,width=250).pack()
+    self.cleanbutton= Button( master, text="Clean!", command=self.clean)
+    self.cleanbutton.pack(side=BOTTOM)
+  def updateMsg( self, argstring):
+    self.msgvar.set(argstring)
+  def composeString( self ):
+    flag=''
+    if self.var.get == 'debug':
+      flag='CXXFLAGS="'+ self.cxxflags[self.var.get()]+'"'
+    polOrder=int( self.polord.get())
+    idxs = self.listbox.curselection()
+    for ind in idxs:
+      index=int(ind)
+      p=self.programms[index]
+      outfile=p+'_make.out'
+      execstring= 'make '+flag+' POLORDER='+ str(polOrder)+' '+p+' &>'+outfile
+      self.updateMsg(execstring)
+      compiled=self.makecall(( execstring , outfile ))
+      if compiled == 0:
+        self.compiled.append( p ) 
+        self.updateMsg('succeeded')
+    pickle.dump( self.compiled, open("compiled.p","wb"))
+  def makecall(self, stringtuple):
+    execstring=stringtuple[0]
+    print(execstring)
+    outfile=stringtuple[1]
+    #subprocess.call([terminal+' -e "'+execstring+'"' ],shell=True)  
+    self.updateMsg( execstring )
+    c = subprocess.call([execstring], shell=True)
+    if c == 0:
+      subprocess.call(['rm '+outfile],shell=True)
+    else:
+      res=self.scanoutfile(outfile)
+      subprocess.call([terminal+' -e "vi '+outfile+'"' ],shell=True)  
+      subprocess.call([terminal+' -e "vi +'+res[1]+' '+res[0]+'"' ],shell=True) 
+    return c
+  def makeit(self):
+    self.composeString()
+  def clean(self):
+    self.updateMsg('clean!')
+    subprocess.call(['make clean'],shell=True)
+    self.compiled=[]
+    pickle.dump( self.compiled, open("compiled.p","wb"))
+  def scanoutfile(self,filename):
+    myfile=open(filename)
+    for line in myfile:
+      if line.find('error') !=-1:
+        splitline=line.split(":")
+        break
+    return (splitline[0], splitline[1])
---
+++ src/scripts/pfrunner.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+import subprocess,sys,pickle,time
+from Tkinter import *
+terminal="xfce4-terminal"
+
+#==================================
+#the Runner class
+#==================================
+class PhasefieldRunner:
+  def __init__(self, master):
+    listframe=Frame(master)
+    listframe.pack()
+    paramframe=Frame(master)
+    paramframe.pack()
+    entryframe=Frame(master)
+    entryframe.pack()
+    messageframe=Frame(master)
+    messageframe.pack(side=BOTTOM)
+    self.programms=pickle.load(open("compiled.p","rb"))
+    self.params=['phasefield.delta','phasefield.mu1','phasefield.mu2','phasefield.endTime', 'fixedTimeStep', 
+    'fem.prefix','phasefield.startLevel','phasefield.alpha','fem.ode.odesolver']
+    self.defaults=['0.01','0.1','0.1','1','0','','2', '0','IM']
+    self.paramEntries=self.makeform(paramframe,self.params)
+    self.listbox = Listbox( listframe )
+    self.listbox.pack(side=LEFT)
+    for item in self.programms:
+      self.listbox.insert(END, item)
+    self.make=Button( entryframe, text="Run", command=self.runit)
+    self.make.pack(side=RIGHT)
+    self.scrvar=IntVar()
+    scr=Radiobutton(entryframe,text='screen',variable=self.scrvar,value=1).pack() 
+    scr=Radiobutton(entryframe,text='show',variable=self.scrvar,value=0).pack() 
+  def makeform( self , root ,fields):
+    entries={}
+    count=0
+    for field in fields:
+      row=Frame(root)
+      lab=Label(row,text=field)
+      ent=Entry(row)
+      row.pack(side=TOP,fill=X)
+      lab.pack(side=LEFT)
+      ent.pack(side=RIGHT,fill=X,expand=YES)
+      ent.insert(0,self.defaults[count])
+      entries.update({field : ent})
+      count+=1
+    return entries
+  def composeString( self ):
+    myday=time.strftime("%d_%m_%Y") 
+    idxs = self.listbox.curselection()
+    index=int(idxs[0])
+    p=self.programms[index]
+    outfile='./Data'+myday+'/'+self.paramEntries['fem.prefix'].get()
+    paramstring=''
+    for key in self.paramEntries:
+      if key =='fem.prefix':
+        paramstring+=key+':'+outfile+'_'+p+' '
+      else:
+        paramstring+=key+':'+self.paramEntries[key].get()+' '
+    execstring ='./'+p+' '+paramstring+'paramfile:parameter_gui '
+    return ( execstring , outfile )
+  def runcall(self, stringtuple):
+    execstring=stringtuple[0]
+    print(execstring)
+    outfile=stringtuple[1]
+    print( outfile )
+    print( self.scrvar.get()) 
+    if self.scrvar.get() == 0: 
+      c = subprocess.call([execstring], shell=True)
+    else:
+      c = subprocess.call([terminal+' -e "screen '+execstring+'"'], shell=True)
+      subprocess.call(['screen -d'],shell=True)
+  def runit(self):
+    self.runcall(self.composeString())
+
