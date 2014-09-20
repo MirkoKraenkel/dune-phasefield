5b780ad4a641e497b1425a5c1470b7f1e238bd25
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-09-20 14:16:02 +0200

    several corrections

--- src/problems/mixedscheme/datareadproblem.hh
+++ src/problems/mixedscheme/datareadproblem.hh
@@ -47,9 +47,8 @@ public:
     delta_(Fem::Parameter::getValue<double>( "phasefield.delta" )),
     rho_( Fem::Parameter::getValue<double> ("phasefield.rho0")),
     phiscale_(Fem::Parameter::getValue<double> ("phiscale")),
-    datafilename_( Fem::Parameter::getValue<std::string>("phasefield.datafile","data.dat")),
+    datafilename_( Fem::Parameter::getValue<std::string>("phasefield.datafile","rhoInvdata.dat")),
     points_(0.),
-    values_(0.),
     thermodyn_()
     {
       readDataFile( datafilename_);
@@ -61,7 +60,7 @@ public:
      std::ifstream bubblefile;
      std::string tmp;
      bubblefile.open(filename.c_str());
-     double a;
+     double a;//x,rho,phi,sig;
      
      if(! bubblefile )
      {
@@ -70,42 +69,41 @@ public:
      }
      else
      {
-      while(  !bubblefile.eof())
-        {
+    
+      while(!bubblefile.eof())
+      {
           bubblefile>>a;
           points_.push_back(a);
           bubblefile>>a;
-          values_.push_back(a);
+          values_[0].push_back(a);
+          bubblefile>>a;
+          values_[1].push_back(a);
+          bubblefile>>a;
+          values_[2].push_back(a);
         }
-     
+      std::cout<<"/n/n------------size "<<points_.size()<<"\n"; 
       bubblefile.close();
-      if(0)
-      for( int i=0 ;(i+1)<points_.size();++i)
-       {
-        std::cout<<"x["<<i<<"]="<<points_[i]<<" , ";
-        std::cout<<"y["<<i<<"]="<<values_[i]<<"\n";
-        }
     }
   }
 
   inline int findpoint( double x) const
   {
-    for( int i=0; (i+1)<points_.size();++i)
-       if( x <  points_[i] && i>0)
-        return i-1;
-     
-    std::cout<<"point not in data x must be in ( "<<points_[0]<<" , "<<points_[points_.size()-1]<<"\n";
+    for( int i=0; (i+1)<(points_.size()-1);++i)
+       if( x>=points_[i] && x <=  points_[i+1] )
+        return i;
+   
+    std::cout<<"point "<< x <<" not in data x must be in ( "<<points_[0]<<" , "<<points_[points_.size()-2]<<"\n";
     abort();
     return -1;
   } 
   
-  inline double linearInterpolation(double x) const
+  inline double linearInterpolation(double x,int component) const
   {
     int i=findpoint( x );
     double x1=points_[i];
     double x2=points_[i+1];
-    double y1=values_[i];
-    double y2=values_[i+1];
+    double y1=values_[component][i];
+    double y2=values_[component][i+1];
     double h=x2-x1;
     return (x-x1)/h *y2 +(x2-x)/h*y1;
    }
@@ -160,7 +158,8 @@ public:
   const double phiscale_;
   std::string datafilename_;
   std::vector<double> points_;
-  std::vector<double> values_;
+  std::array<std::vector<double>,3> values_;
+  
   const ThermodynamicsType thermodyn_;
     
 };
@@ -189,11 +188,12 @@ inline void DataReadProblem<GridType,RangeProvider>
   double cosx=cos(2*M_PI*x);
   double sinx=sin(2*M_PI*x);
     
-  double rho=rho_;
+  double rho=linearInterpolation(x,0);
+  double rhoInv=1./rho;
    //double v=sinx*cost;
    double v=0;
    //rho
-   res[0]=2.5;
+   res[0]=rho;
    //v
    for(int i=1;i<=dimension;i++)
    {
@@ -201,16 +201,18 @@ inline void DataReadProblem<GridType,RangeProvider>
    }
    if(dimension==2)
      res[2]=0;
-   double  phi=linearInterpolation(x);
+   double  phi=linearInterpolation(x,1);
    res[dimension+1]=phi;
-   
+  double sigma=  linearInterpolation(x,2);
+ 
   double dFdphi= thermodyn_.reactionSource(rho,phi); 
   double dFdrho=thermodyn_.chemicalPotential(rho, phi);
      
    if( dimRange > dimDomain+2)
     {
       //mu
-      res[dimension+2]=0;
+      res[dimension+2]=0.5*v*v+dFdrho-delta_*rhoInv *0.5*sigma*sigma;
+
       //tau
       res[dimension+3]=0;
 
@@ -219,9 +221,9 @@ inline void DataReadProblem<GridType,RangeProvider>
     //sigma
     if(dimension==1)
       {
-        res[dimension+4]=-2*M_PI*sinx*cost*0.05;
+        res[dimension+4]=linearInterpolation(x,2);
       #if RHOMODEL
-        res[dimension+5]=res[dimension+4]*rho;
+        res[dimension+5]=0;
       #endif
       }
      else
