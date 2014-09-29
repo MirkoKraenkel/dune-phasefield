1f924775f3df90d4cc73516747ba9164c089f5b2
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-09-29 14:22:36 +0200

    generate thermo sources and rhs for problem in one script

--- dune/phasefield/modelling/scripts/codegen.py
+++ dune/phasefield/modelling/scripts/codegen.py
@@ -2,19 +2,22 @@ import os,subprocess, sys
 folders = { 1:'../ConstRhoSources/' , 2:'../InvRhoSources/', 3:'../PhasefieldvanderWaalsSources/'}
 files = { 1:'balancedCODEGEN.mpl', 2:'balancedhmodelCODEGEN.mpl', 3:'phasefieldvanderWaalsCODEGEN.mpl'}
 namelist = [' helmholtz', ' reactionSource',' dphireactionSource',' chemicalPotential',' dphichemicalPotential',' drhochemicalPotential',' pressure', ' a']
+namelist2 = [' rhosol', ' gradrho',' gradphi',' musol',' thetasol',' phiSource',' veloSource']
+models={ 1:'balanced',2:'balancedh'}
 number=int(sys.argv[1])
 inputfile=folders[number]+files[number]
-subprocess.call( ['rm maple.c'],shell=True)
+subprocess.call( ['rm maple.cc'],shell=True)
 subprocess.call( ['maple '+inputfile],shell=True )
-f=open( 'maple.c')
-fnew=open( 'maplenew.c','w')
+f=open( 'maple.cc')
+fnew=open( 'maplenew.cc','w')
 flag = False
 for line in f:
   if line[0]=='#':
     fnew.write('\n')
   else:
     newline1 = line.replace('delta','delta_')
-    newline = newline1.replace('alpha','alpha_')
+    newline2 = line.replace('beta', 'beta_') 
+    newline = newline2.replace('alpha','alpha_')
     for name in namelist:
       if newline.find( name ) != -1:
         flag=True
@@ -25,5 +28,30 @@ for line in f:
       flag=False
     else:
       fnew.write( newline )
-subprocess.call( ['mv maplenew.c '+folders[number]+'/maple.c'], shell=True)
-subprocess.call( ['rm maple.c'], shell=True)
+subprocess.call( ['mv maplenew.cc '+folders[number]+'/maple.cc'], shell=True)
+subprocess.call( ['rm maple.cc'], shell=True )
+if number==1 or number==2:
+  filename=models[number]+'.cc'
+  f=open(filename)
+  fnew=open( 'maplenew.cc','w')
+  flag = False
+  for line in f:
+    if line[0]=='#':
+      fnew.write('\n')
+    else:
+      newline1 = line.replace('delta','delta_')
+      newline2 = line.replace('beta', 'beta_') 
+      newline = newline2.replace('alpha','alpha_')
+      for name in namelist2:
+        if newline.find( name ) != -1:
+          flag=True
+          break 
+      if flag:   
+        newline = 'inline double'+name+' ( double x ) const\n'
+        fnew.write(newline)
+        flag=False
+      else:
+        fnew.write( newline )
+  fnew.close()
+  f.close()
+  subprocess.call( ['mv maplenew.cc ../../../../src/problems/mixedscheme/sourceprobCODEGEN/'+filename], shell=True)
