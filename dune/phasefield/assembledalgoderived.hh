347f398397c0cc1244626af639eac609f11c0182
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-10-21 09:00:52 +0200

    added pressure and timestepcontrol

--- dune/phasefield/assembledalgoderived.hh
+++ dune/phasefield/assembledalgoderived.hh
@@ -37,12 +37,11 @@ class AssembledAlgorithm: public PhasefieldAlgorithmBase< GridImp,AlgorithmTrait
   typedef typename Traits :: LinearSolverType LinearSolverType;
   typedef typename Dune::Fem::NewtonInverseOperator< JacobianOperatorType, LinearSolverType > NewtonSolverType; 
   //adaptation
-  //typedef MixedEstimator<DiscreteFunctionType,ModelType> EstimatorType;
-	//typedef Dune::Fem::LocalFunctionAdapter<EstimatorType> EstimatorDataType;
   typedef typename Traits::EstimatorType EstimatorType;
   typedef typename Traits::EstimatorDataType EstimatorDataType;
   typedef typename Traits::IOTupleType IOTupleType;
-  
+  typedef typename BaseType::DataWriterType DataWriterType;
+
   using BaseType::grid_;
   using BaseType::gridPart_;
   using BaseType::space_;
@@ -56,9 +55,12 @@ class AssembledAlgorithm: public PhasefieldAlgorithmBase< GridImp,AlgorithmTrait
     DiscreteOperatorType    dgOperator_;
     BoundaryCorrectionType  boundaryCorrection_;
     DiscreteFunctionType    start_;
+    DiscreteScalarType      pressure_;
     EstimatorType           estimator_;
     EstimatorDataType       estimatorData_;
-
+    double                  surfaceEnergy_;
+    bool                    stepconverged_;
+    int                     maxNewtonIter_;
   public:
   //Constructor
   AssembledAlgorithm( GridType& gridImp):
@@ -66,15 +68,29 @@ class AssembledAlgorithm: public PhasefieldAlgorithmBase< GridImp,AlgorithmTrait
       dgOperator_( *model_ , space()),
       boundaryCorrection_( *model_ , space()),
       start_( "start", space() ),
+      pressure_( "pressure", energyspace()),
       estimator_( solution_, grid_, model()),
-      estimatorData_( "estimator", estimator_, gridPart_, space_.order() )
+      estimatorData_( "estimator", estimator_, gridPart_, space_.order() ),
+      surfaceEnergy_(0.),
+      stepconverged_(false),
+      maxNewtonIter_( Dune::Fem::Parameter::getValue<int>("fem.ode.maxiterations") )
       {
-      start_.clear();
+        start_.clear();
       }
 
-    IOTupleType getDataTuple (){ return IOTupleType( &solution(), &estimatorData_, this->energy());}
-    
-    void initializeSolver(typename BaseType::TimeProviderType& timeProvider){};
+    IOTupleType getDataTuple (){ return IOTupleType( &solution(), &estimatorData_,&pressure_, this->energy());}
+
+    DiscreteScalarType&  getpressure(){ return pressure_;}
+
+    void initializeSolver ( typename BaseType::TimeProviderType& timeProvider){}
+
+    void reduceTimeStep( TimeProviderType& timeProvider, double ldt)
+      {
+        double factor=0.5;
+        timeProvider.provideTimeStepEstimate( factor*ldt);
+        timeProvider.invalidateTimeStep();
+      }
+
 
     void step ( TimeProviderType& timeProvider,
 		  				  int& newton_iterations,
@@ -82,27 +98,38 @@ class AssembledAlgorithm: public PhasefieldAlgorithmBase< GridImp,AlgorithmTrait
 				  		  int& max_newton_iterations,
 					  	  int& max_ils_iterations)
 	  {
-     const double time=timeProvider.time();
-     const double deltaT=timeProvider.deltaT();
-    
+      const double time=timeProvider.time();
+      const double deltaT=timeProvider.deltaT();
       DiscreteFunctionType& U=solution();
-      
+      DiscreteFunctionType& Uold=oldsolution();
       dgOperator_.setPreviousTimeStep(U);
       dgOperator_.setTime(time);
       dgOperator_.setDeltaT(deltaT);
       NewtonSolverType invOp(dgOperator_); 
       start_.clear();
-   
+
       invOp(start_,U);
       //boundaryCorrection_(U);
       //reset overall timer
-      overallTimer_.reset(); 
-     
-		  newton_iterations     = invOp.iterations();
+      overallTimer_.reset();
+
+      newton_iterations     = invOp.iterations();
       ils_iterations        = invOp.linearIterations();;
       max_newton_iterations = std::max(max_newton_iterations,newton_iterations);
       max_ils_iterations    = std::max(max_ils_iterations,ils_iterations);
-	}
+
+      if( !invOp.converged() || newton_iterations > maxNewtonIter_)
+        {
+          std::cout<<"NewtonIterations: "<<newton_iterations<<" ( "<<maxNewtonIter_<<" )  with deltaT="<<deltaT;
+          reduceTimeStep( timeProvider , deltaT );
+          std::cout<<" - reduced Timestep="<<timeProvider.deltaT()<<"\n";
+          U.assign(Uold);
+        }
+      else
+        {
+          timeProvider.provideTimeStepEstimate( deltaT);
+        }
+    }
 	  
   void estimateMarkAdapt( AdaptationManagerType& am)
   {
@@ -130,7 +157,6 @@ class AssembledAlgorithm: public PhasefieldAlgorithmBase< GridImp,AlgorithmTrait
   using BaseType::gridSize;
   using BaseType::oldsolution;
   using BaseType::solution;
-
   using BaseType::energy;
   using BaseType::dataPrefix;
   using BaseType::problem;
@@ -140,23 +166,40 @@ class AssembledAlgorithm: public PhasefieldAlgorithmBase< GridImp,AlgorithmTrait
 
   template<class Stream>
   void writeEnergy( TimeProviderType& timeProvider,
-                    Stream& str)
+                    Stream& str,
+                    double& difference)
   {
     DiscreteScalarType* totalenergy = energy();
+    DiscreteScalarType&  pressure=getpressure();
     { 
       double kineticEnergy;
       double chemicalEnergy; 
       double surfaceEnergy;
-      double energyIntegral =energyconverter(solution(),model(),*totalenergy,kineticEnergy,chemicalEnergy,surfaceEnergy);
+      double energyIntegral =energyconverter(solution(),model(),*totalenergy,pressure,kineticEnergy,chemicalEnergy,surfaceEnergy);
       str<<std::setprecision( 20 )<<timeProvider.time()<<"\t"<<energyIntegral<<"\t"<<chemicalEnergy<<"\t"<<kineticEnergy<<"\t"<<surfaceEnergy<<"\n";
-
+      difference=surfaceEnergy_-surfaceEnergy;
+ //     std::cout<<"surfaceEnergy Change="<<surfaceEnergy_-surfaceEnergy<<"\n"; 
+      surfaceEnergy_=surfaceEnergy;
     } 
   }
  
+  virtual void writeData( DataWriterType& eocDataOutput,
+                          TimeProviderType& timeProvider,
+                          const bool reallyWrite )
+	{
+    DiscreteScalarType* totalenergy = energy();
+    DiscreteScalarType&  pressure=getpressure();
+    if( reallyWrite )
+		{
 
-
-
-
+	    double kineticEnergy;
+      double chemicalEnergy;
+      double surfaceEnergy;
+      double energyIntegral =energyconverter(solution(),model(),*totalenergy,pressure,kineticEnergy,chemicalEnergy,surfaceEnergy);
+   }
+	 // write the data
+   eocDataOutput.write( timeProvider );
+	}
 };
 }//end namespace Dune
 
