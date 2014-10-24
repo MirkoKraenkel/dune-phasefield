458f55d949b552bd16976f3ee263a61c645ce7c1
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-10-24 15:40:32 +0200

    added 2d

--- dune/fem/operator/assembled/Codegen/generateCouplings.py
+++ dune/fem/operator/assembled/Codegen/generateCouplings.py
@@ -1,6 +1,8 @@
 import os, subprocess, sys
 rho='rho'
 v='v'
+vx='vx'
+vy='vy'
 u='u'
 phi='phi'
 mu='mu'
@@ -9,7 +11,6 @@ sigmax='sigmax'
 sigmay='sigmay'
 
 
-f=open('elementCouplings1d_CODEGEN.c','w')
 variables1d={ rho: 0 , v: 1 ,phi: 2, mu : 3 , tau : 4, sigmax:  5 }
 varlist=list(variables1d.keys())
 varlist.sort()
@@ -41,6 +42,46 @@ i=0
 for cp in interseccouplings1d:
   v1=variables1d[ cp[0] ]
   v2=variables1d[ cp[1] ]
+  newline='intersectionCouplings_[ '+str( i )+' ] = std::make_pair('+str(v1)+','+str(v2)+');//('+cp[0]+','+cp[1]+')\n'
+  f.write(newline)
+  i=i+1
+f.close()
+
+variables2d={ rho: 0 , vx: 1 , vy:2 ,phi: 3, mu : 4 , tau : 5, sigmax:  6 ,sigmay: 7}
+varlist=list(variables2d.keys())
+varlist.sort()
+
+elemcouplings2d=[ ( rho , rho ),( rho , vx ),(rho,vy),
+            ( vx , rho ),( vx , vx ),( vx , vy ), ( vx, phi ),( vx , mu ),( vx , tau ),
+            ( vy , rho ),( vy , vx ),( vy , vy ), ( vy, phi ),( vy , mu ),( vy , tau ),
+            ( phi , rho ),( phi , vx ),( phi , vy ), ( phi,phi),(phi,tau),
+            ( mu, rho),(mu,vx),(mu,vy),(mu,phi),(mu,mu),
+            ( tau,rho),(tau,phi),(tau,tau),(tau,sigmax),(tau,sigmay),
+            (sigmax,phi),(sigmax,sigmax),
+            (sigmay,phi),(sigmay,sigmay)
+]
+interseccouplings2d=[  ( rho , rho ), ( rho , vx ),( rho , vy ),( rho , mu),
+            ( vx , rho ),( vx , vx ),(vx,vy), ( vx, phi ),( vx , mu ),( vx , tau ),
+            ( vy , rho ),( vy , vx ),(vy,vy), ( vy, phi ),( vy , mu ),( vy , tau ),
+            ( phi , vx ),( phi , vy ), ( phi,phi),
+            (tau,tau),(tau,sigmax),(tau,sigmay),
+            (sigmax,phi),(sigmax,sigmax),
+            (sigmay,phi),(sigmay,sigmay)
+]
+f=open('elementCouplings2d_CODEGEN.c','w')
+i=0
+for cp in elemcouplings2d:
+  v1=variables2d[ cp[0] ]
+  v2=variables2d[ cp[1] ]
+  newline='elementCouplings_[ '+str( i )+' ] = std::make_pair('+str(v1)+','+str(v2)+');//('+cp[0]+','+cp[1]+')\n'
+  f.write(newline)
+  i=i+1
+f.close()
+f=open('intersectionCouplings2d_CODEGEN.c','w')
+i=0
+for cp in interseccouplings2d:
+  v1=variables2d[ cp[0] ]
+  v2=variables2d[ cp[1] ]
   newline='intersectionCouplings_[ '+str( i )+' ] = std::make_pair('+str(v1)+','+str(v2)+');//('+cp[0]+','+cp[1]+')\n'
   f.write(newline)
   i=i+1
