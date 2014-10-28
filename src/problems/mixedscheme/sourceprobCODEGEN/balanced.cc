6beb9f3e1c511f6f776a92c6f153fcc0394e3589
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2014-10-28 15:36:44 +0100

    update

--- src/problems/mixedscheme/sourceprobCODEGEN/balanced.cc
+++ src/problems/mixedscheme/sourceprobCODEGEN/balanced.cc
@@ -74,15 +74,15 @@ inline double gradphi ( double x ) const
 inline double thetasol ( double x ) const
 {
   double t1;
-  double t19;
-  double t23;
-  double t24;
+  double t21;
+  double t25;
+  double t26;
   double t3;
-  double t35;
   double t37;
-  double t38;
-  double t49;
+  double t39;
+  double t40;
   double t5;
+  double t51;
   double t6;
   double t7;
   {
@@ -91,17 +91,17 @@ inline double thetasol ( double x ) const
     t5 = 0.5*t3+0.5;
     t6 = t5*t5;
     t7 = t6*t5;
-    t19 = t6*t6;
-    t23 = 30.0*t19-60.0*t7+30.0*t6;
-    t24 = t19*t5;
-    t35 = exp((0.4000000004E1-18.0*t24+45.0*t19-30.0*t7)/(-0.9E1*t24+0.225E2*
-t19-0.15E2*t7+3.0));
-    t37 = log(t35);
-    t38 = t35*t37;
-    t49 = t3*t3;
-    return((8.0*t7+6.0*(2.0*alpha_-2.0)*t6+4.0*(-3.0*alpha_+1.0)*t5)*t1+beta_*(t23
-*(-0.25E1*t35+0.15E1*t38+0.15E1)-t23*(-7.0*t35+3.0*t38+0.995940263E1))+0.1E1*t1
-*t3*(1.0-t49));
+    t21 = t6*t6;
+    t25 = 30.0*t21-60.0*t7+30.0*t6;
+    t26 = t21*t5;
+    t37 = exp((0.4000000004E1-18.0*t26+45.0*t21-30.0*t7)/(-0.9E1*t26+0.225E2*
+t21-0.15E2*t7+3.0));
+    t39 = log(t37);
+    t40 = t37*t39;
+    t51 = t3*t3;
+    return(2.0*A_*(4.0*t7+3.0*(2.0*alpha_-2.0)*t6+2.0*(-3.0*alpha_+1.0)*t5)*t1+
+beta_*(t25*(-0.25E1*t37+0.15E1*t40+0.2921601062E1)-t25*(-7.0*t37+3.0*t40+
+0.1138100368E2))+0.1E1*t1*t3*(1.0-t51));
   }
 }
 
@@ -116,16 +116,16 @@ inline double phiSource ( double x ) const
   double t25;
   double t26;
   double t3;
-  double t38;
-  double t42;
-  double t43;
+  double t40;
+  double t44;
+  double t45;
   double t5;
-  double t54;
   double t56;
-  double t57;
+  double t58;
+  double t59;
   double t6;
-  double t68;
   double t7;
+  double t70;
   double t8;
   {
     t1 = 1/delta_;
@@ -141,17 +141,17 @@ inline double phiSource ( double x ) const
     t24 = 0.5*t22+0.5;
     t25 = t24*t24;
     t26 = t25*t24;
-    t38 = t25*t25;
-    t42 = 30.0*t38-60.0*t26+30.0*t25;
-    t43 = t38*t24;
-    t54 = exp((0.4000000004E1-18.0*t43+45.0*t38-30.0*t26)/(-0.9E1*t43+0.225E2*
-t38-0.15E2*t26+3.0));
-    t56 = log(t54);
-    t57 = t56*t54;
-    t68 = t22*t22;
-    return(((8.0*t26+6.0*(2.0*alpha_-2.0)*t25+4.0*(-3.0*alpha_+1.0)*t24)*t1+beta_*
-(t42*(-0.25E1*t54+0.15E1*t57+0.15E1)-t42*(-7.0*t54+3.0*t57+0.995940263E1))+
-0.1E1*t1*t22*(1.0-t68))/t20);
+    t40 = t25*t25;
+    t44 = 30.0*t40-60.0*t26+30.0*t25;
+    t45 = t40*t24;
+    t56 = exp((0.4000000004E1-18.0*t45+45.0*t40-30.0*t26)/(-0.9E1*t45+0.225E2*
+t40-0.15E2*t26+3.0));
+    t58 = log(t56);
+    t59 = t58*t56;
+    t70 = t22*t22;
+    return((2.0*A_*(4.0*t26+3.0*(2.0*alpha_-2.0)*t25+2.0*(-3.0*alpha_+1.0)*t24)*t1
++beta_*(t44*(-0.25E1*t56+0.15E1*t59+0.2921601062E1)-t44*(-7.0*t56+3.0*t59+
+0.1138100368E2))+0.1E1*t1*t22*(1.0-t70))/t20);
   }
 }
 
@@ -191,33 +191,33 @@ inline double veloSource ( double x ) const
 {
   double t1;
   double t10;
-  double t122;
-  double t22;
-  double t26;
-  double t27;
+  double t124;
+  double t24;
+  double t28;
+  double t29;
   double t3;
-  double t31;
-  double t35;
-  double t36;
+  double t33;
+  double t37;
   double t38;
   double t4;
   double t40;
-  double t41;
+  double t42;
+  double t43;
   double t5;
-  double t57;
+  double t59;
   double t6;
-  double t61;
-  double t64;
-  double t67;
-  double t70;
-  double t71;
-  double t78;
-  double t79;
+  double t63;
+  double t66;
+  double t69;
+  double t72;
+  double t73;
   double t8;
-  double t86;
+  double t80;
+  double t81;
+  double t88;
   double t9;
-  double t90;
-  double t95;
+  double t92;
+  double t97;
   {
     t1 = 1/delta_;
     t3 = tanh(x*t1);
@@ -227,33 +227,33 @@ inline double veloSource ( double x ) const
     t8 = 0.5*t3+0.5;
     t9 = t8*t8;
     t10 = t9*t8;
-    t22 = t9*t9;
-    t26 = 30.0*t22-60.0*t10+30.0*t9;
-    t27 = t22*t8;
-    t31 = 0.4000000004E1-18.0*t27+45.0*t22-30.0*t10;
-    t35 = -0.9E1*t27+0.225E2*t22-0.15E2*t10+3.0;
-    t36 = 1/t35;
-    t38 = exp(t31*t36);
-    t40 = log(t38);
-    t41 = t40*t38;
-    t57 = beta_*t22;
-    t61 = t22*t5*t1;
-    t64 = t10*t5*t1;
-    t67 = t9*t5*t1;
-    t70 = (-0.45E2*t61+0.9E2*t64-0.45E2*t67)*t36;
-    t71 = t35*t35;
-    t78 = t31/t71*(-0.225E2*t61+0.45E2*t64-0.225E2*t67);
-    t79 = t70-t78;
-    t86 = beta_*t10;
-    t90 = t40*t5*t1;
-    t95 = beta_*t9;
-    t122 = -0.15E2*t57*t6+9.0*t79*beta_*t27+0.225E2*t40*beta_*t61+0.3E2*t86*t6
--0.45E2*t86*t90-0.225E2*t57*t79-0.15E2*t95*t6+0.225E2*t95*t90+15.0*t86*t79+3.0*
-t70-3.0*t78+0.6E2*t61-0.45E2*t22*t40*t6-18.0*t27*t79-0.12E3*t64+0.9E2*t10*t40*
-t6+45.0*t22*t79+0.6E2*t67-0.45E2*t9*t40*t6-30.0*t10*t79;
-    return(-0.5*t6*((8.0*t10+6.0*(2.0*alpha_-2.0)*t9+4.0*(-3.0*alpha_+1.0)*t8)*t1
-+beta_*(t26*(-0.25E1*t38+0.15E1*t41+0.15E1)-t26*(-7.0*t38+3.0*t41+0.995940263E1)
-)+0.1E1*t1*t3*t5)+t38*t122);
+    t24 = t9*t9;
+    t28 = 30.0*t24-60.0*t10+30.0*t9;
+    t29 = t24*t8;
+    t33 = 0.4000000004E1-18.0*t29+45.0*t24-30.0*t10;
+    t37 = -0.9E1*t29+0.225E2*t24-0.15E2*t10+3.0;
+    t38 = 1/t37;
+    t40 = exp(t33*t38);
+    t42 = log(t40);
+    t43 = t42*t40;
+    t59 = beta_*t24;
+    t63 = t24*t5*t1;
+    t66 = t10*t5*t1;
+    t69 = t9*t5*t1;
+    t72 = (-0.45E2*t63+0.9E2*t66-0.45E2*t69)*t38;
+    t73 = t37*t37;
+    t80 = t33/t73*(-0.225E2*t63+0.45E2*t66-0.225E2*t69);
+    t81 = t72-t80;
+    t88 = beta_*t10;
+    t92 = t42*t5*t1;
+    t97 = beta_*t9;
+    t124 = -0.15E2*t59*t6+9.0*t81*beta_*t29+0.225E2*t42*beta_*t63+0.3E2*t88*t6
+-0.45E2*t88*t92-0.225E2*t59*t81-0.15E2*t97*t6+0.225E2*t97*t92+15.0*t88*t81+3.0*
+t72-3.0*t80+0.6E2*t63-0.45E2*t24*t42*t6-18.0*t29*t81-0.12E3*t66+0.9E2*t10*t42*
+t6+45.0*t24*t81+0.6E2*t69-0.45E2*t9*t42*t6-30.0*t10*t81;
+    return(-0.5*t6*(2.0*A_*(4.0*t10+3.0*(2.0*alpha_-2.0)*t9+2.0*(-3.0*alpha_+1.0)*
+t8)*t1+beta_*(t28*(-0.25E1*t40+0.15E1*t43+0.2921601062E1)-t28*(-7.0*t40+3.0*t43+
+0.1138100368E2))+0.1E1*t1*t3*t5)+t40*t124);
   }
 }
 
