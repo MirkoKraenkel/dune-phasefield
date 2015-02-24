fc249cbace0c3bae71670210d826a37edc79c9b3
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-02-24 15:37:40 +0100

    removed unused variable warning

--- dune/phasefield/assembledalgoderived.hh
+++ dune/phasefield/assembledalgoderived.hh
@@ -102,7 +102,7 @@ class AssembledAlgorithm: public PhasefieldAlgorithmBase< GridImp,AlgorithmTrait
       const double time=timeProvider.time();
       const double deltaT=timeProvider.deltaT();
       DiscreteFunctionType& U=solution();
-      DiscreteFunctionType& Uold=oldsolution();
+      //DiscreteFunctionType& Uold=oldsolution();
       dgOperator_.setPreviousTimeStep(U);
       dgOperator_.setTime(time);
       dgOperator_.setDeltaT(deltaT);
