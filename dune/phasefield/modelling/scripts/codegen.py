5d0e6ac88ad3ee9208db5e36d552630860aa8a3b
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-02-24 14:57:21 +0100

    added models

--- dune/phasefield/modelling/scripts/codegen.py
+++ dune/phasefield/modelling/scripts/codegen.py
@@ -1,9 +1,11 @@
 import os,subprocess, sys
-folders = { 1:'../ConstRhoSources/' , 2:'../InvRhoSources/', 3:'../PhasefieldvanderWaalsSources/', 4:'../ConstRhoSources/',5:'../InvRhoSources/'}
-files = { 1:'balancedCODEGEN.mpl', 2:'balancedhmodelCODEGEN.mpl', 3:'phasefieldvanderWaalsCODEGEN.mpl', 4:'balancedCoquelCODEGEN.mpl', 5:'balancedhmodelCoqCODEGEN.mpl'}
+folders = { 1:'../ConstRhoSources/' , 2:'../InvRhoSources/', 3:'../PhasefieldvanderWaalsSources/', 
+4:'../ConstRhoSources/',5:'../InvRhoSources/',6:'../TaitSources/'}
+files = { 1:'balancedCODEGEN.mpl', 2:'balancedhmodelCODEGEN.mpl', 3:'phasefieldvanderWaalsCODEGEN.mpl', 
+4:'balancedCoquelCODEGEN.mpl', 5:'balancedhmodelCoqCODEGEN.mpl', 6:'taitCODEGEN.mpl'}
 namelist = [' helmholtz', ' reactionSource',' dphireactionSource',' chemicalPotential',' dphichemicalPotential',' drhochemicalPotential',' pressure', ' a']
 namelist2 = [' rhosol', ' gradrho',' gradphi',' musol',' thetasol',' phiSource',' veloSource']
-models={ 1:'balanced',2:'balancedh',3:'vdW', 4:'balCoq',5:'balhcoq'}
+models={ 1:'balanced',2:'balancedh',3:'vdW', 4:'balCoq',5:'balhcoq', 6:'tait'}
 number=int(sys.argv[1])
 inputfile=folders[number]+files[number]
 filename=models[number]+'.cc'
