b6837db4b84b27bfc4c135155c2406deb9ba977b
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-04-28 16:20:51 +0200

    added couplings for split algo

---
+++ dune/fem/operator/assembled/Codegen/acisCouplings1d_CODEGEN.c
@@ -0,0 +1,4 @@
+intersectionCouplings_[ 0 ] = std::make_pair(0,0);//(phi,phi)
+intersectionCouplings_[ 1 ] = std::make_pair(1,0);//(tau,phi)
+intersectionCouplings_[ 2 ] = std::make_pair(1,2);//(tau,sigmax)
+intersectionCouplings_[ 3 ] = std::make_pair(2,0);//(sigmax,phi)
---
+++ dune/fem/operator/assembled/Codegen/nvstisCouplings1d_CODEGEN.c
@@ -0,0 +1,6 @@
+intersectionCouplings_[ 0 ] = std::make_pair(0,0);//(rho,rho)
+intersectionCouplings_[ 1 ] = std::make_pair(0,1);//(rho,v)
+intersectionCouplings_[ 2 ] = std::make_pair(0,2);//(rho,mu)
+intersectionCouplings_[ 3 ] = std::make_pair(1,0);//(v,rho)
+intersectionCouplings_[ 4 ] = std::make_pair(1,1);//(v,v)
+intersectionCouplings_[ 5 ] = std::make_pair(1,2);//(v,mu)
