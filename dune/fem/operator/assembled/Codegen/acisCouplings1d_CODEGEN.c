ba8d4186df34106f019904a31c6b20e5e552d263
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-05-06 15:41:25 +0200

    use arrays

--- dune/fem/operator/assembled/Codegen/acisCouplings1d_CODEGEN.c
+++ dune/fem/operator/assembled/Codegen/acisCouplings1d_CODEGEN.c
@@ -1,4 +1,4 @@
-intersectionCouplings_.push_back( std::make_pair(0,0));//(phi,phi)
-intersectionCouplings_.push_back( std::make_pair(1,0));//(tau,phi)
-intersectionCouplings_.push_back( std::make_pair(1,2));//(tau,sigmax)
-intersectionCouplings_.push_back( std::make_pair(2,0));//(sigmax,phi)
+intersectionCouplings_[ 0 ] = std::make_pair(0,0);//(phi,phi)
+intersectionCouplings_[ 1 ] = std::make_pair(1,0);//(tau,phi)
+intersectionCouplings_[ 2 ] = std::make_pair(1,2);//(tau,sigmax)
+intersectionCouplings_[ 3 ] = std::make_pair(2,0);//(sigmax,phi)
