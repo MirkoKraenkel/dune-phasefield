6d04f714b52a46d87001ec6da1768dc39ff6a103
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-05-08 17:10:52 +0200

    make independent of dimension, wrapper classes for split scheme

--- src/problems/mixedscheme/mixproblem2d.hh
+++ src/problems/mixedscheme/mixproblem2d.hh
@@ -1,9 +1,8 @@
-#ifndef MIXPRONLEM_HH
-#define MIXPRONLEM_HH
+#ifndef PHASEFIELD_MIXPROBLEM_HH
+#define PHASEFIELD_MIXPROBLEM_HH
 #include <dune/common/version.hh>
 
 // dune-fem includes
-#include <dune/fem/misc/linesegmentsampler.hh>
 #include <dune/fem/io/parameter.hh>
 #include <dune/fem/space/common/functionspace.hh>
 
@@ -14,13 +13,6 @@
 #else
 #include <dune/phasefield/modelling/thermodynamicsbalancedphases.hh>
 #endif
-
-
-//#include <dune/phasefield/modelling/thermodynamicsfreistuehler.hh>
-//#include <dune/phasefield/modelling/thermodynamicsvanderWaals.hh>
-
-
-//#include <dune/fem/probleminterfaces.hh>
 
 #include <dune/fem-dg/models/defaultprobleminterfaces.hh>
 
@@ -33,26 +25,25 @@ class MixProblem : public EvolutionProblemInterface<
 {
  
 public:
-  enum{ dimension = GridType::dimensionworld };
-  enum{ dimDomain = dimension };
-  enum{ phasefieldId = dimension + 1 };
-  enum{ dimRange=RangeProvider::rangeDim};
-  typedef Fem::FunctionSpace<typename GridType::ctype, double, GridType::dimensionworld,dimRange > FunctionSpaceType ;
+  constexpr static int dimension = GridType::dimensionworld;
+  constexpr static int phasefieldId = dimension + 1 ;
+  constexpr static int dimRange=RangeProvider::rangeDim;
   
+  using FunctionSpaceType = typename Fem::FunctionSpace<typename GridType::ctype,
+                                                        double,
+                                                        GridType::dimensionworld,
+                                                        dimRange >;
 
+  using DomainFieldType = typename FunctionSpaceType :: DomainFieldType;
+  using DomainType      = typename FunctionSpaceType :: DomainType;
+  using RangeFieldType  = typename FunctionSpaceType :: RangeFieldType;
+  using RangeType       = typename FunctionSpaceType :: RangeType;
 
-  typedef typename FunctionSpaceType :: DomainFieldType   DomainFieldType;
-  typedef typename FunctionSpaceType :: DomainType        DomainType;
-  typedef typename FunctionSpaceType :: RangeFieldType    RangeFieldType;
-  typedef typename FunctionSpaceType :: RangeType         RangeType;
-
-//   typedef TestThermodynamics ThermodynamicsType;
-  typedef BalancedThermodynamics ThermodynamicsType; 
+  using ThermodynamicsType = BalancedThermodynamics;
 
   MixProblem() : 
     myName_( "Mixedtest Heatproblem" ),
     endTime_ ( Fem::Parameter::getValue<double>( "phasefield.endTime",1.0 )), 
-    mu_( Fem::Parameter :: getValue< double >( "phasefield.mu1" )),
     delta_(Fem::Parameter::getValue<double>( "phasefield.delta" )),
     A_(Fem::Parameter::getValue<double>("phasefield.A")),
     rho_( Fem::Parameter::getValue<double> ("phasefield.rho0")),
@@ -102,12 +93,10 @@ public:
   void paraview_conv2prim() const {}
   std::string description() const;
  
-  inline double mu() const { abort(); return mu_; }
   inline double delta() const{return thermodyn_.delta();}
   protected:
   const std::string myName_;
   const double endTime_;
-  const double mu_;
   const double delta_;
   double A_;
   double rho_;
@@ -139,10 +128,6 @@ inline void MixProblem<GridType,RangeProvider>
   double y=0;
   if(dimension==2)
     y=arg[1];
-  double cosx=cos(2*M_PI*x);
-  double cosy=cos(2*M_PI*y);
-  double sinx=sin(2*M_PI*x);
-  double siny=sin(2*M_PI*y);
   
   double rho=rho_;
   double rho_inv=1./rho_;
@@ -150,57 +135,36 @@ inline void MixProblem<GridType,RangeProvider>
   //rho
   res[0]= rho;
   //v
-  for(int i=1;i<=dimension;i++)
-   {
-    res[i]=v;
-   }
+  for(int ii = 0; ii < dimension ; ++ii )
+    res[ii+1]=v;
   
-  if(dimension==2)
-     res[2]=0;
-   
-   double  phi=0.05*cosx*cosy+0.5;
-   res[dimension+1]=phi;
+  double phi=0.05;
+
+  for( int ii = 0; ii < dimension ; ++ii)
+    phi*=cos(2*M_PI*arg[ii]);
+
+  phi+=0.5;
+  res[dimension+1]=phi;
 
 
 #if MIXED     
-  double dFdphi= thermodyn_.reactionSource(rho,phi); 
-  double dFdrho=thermodyn_.chemicalPotential(rho, phi);
-  double sigma_x=-2*M_PI*sinx*0.05;
-  double sigma_y=-2*M_PI*siny*0.05;
-#if RHOMODEL
-  //mu
-  res[dimension+2]=0.5*v*v+dFdrho+thermodyn_.delta()*thermodyn_.h2prime(rho)*(sigma_x*sigma_x+sigma_y*sigma_y)*0.5;
-  //tau
-  res[dimension+3]=thermodyn_.delta()*4*0.05*M_PI*M_PI*rho_inv*cosx+dFdphi;
-  //sigma_x
-  res[dimension+4]=sigma_x;
-#else
-  //mu
-  res[dimension+2]=0.5*v*v+dFdrho;
-  //tau
-  res[dimension+3]=thermodyn_.delta()*0.1*0.25*M_PI*M_PI*cosx*cosy+dFdphi;
-  //sigma_x
-  res[dimension+4]=-M_PI*sinx*cosy*0.05*0.5;
-#endif
-#if LAMBDASCHEME 
-  //alpha_x
-  res[dimension+4+dimension]=res[dimension+4]*thermodyn_.h2(rho);
-#endif
-  if(dimension==2)
+  for( int ii = 0 ; ii<dimension; ++ ii)
     {
-      //sigma_y
-      res[dimension+5]=sigma_y;
+      res[dimension+4+ii]=-2*M_PI*sin(2*M_PI*arg[ii]);
 #if LAMBDASCHEME
-      //alpha_y
-      res[dimension+7]=thermodyn_.h2(rho)*res[dimension+5];
+      res[2*dimension+4+ii]=thermodyn_.h2(rho)*res[dimension+4+ii];
 #endif
     }
+  //tau
+  res[dimension+3]=0;
+  //mu
+  res[dimension+2]=0;
 #else
 
 #if NONCONTRANS
 #else
-      res[1]*=res[0];
-      res[2]*=res[0];
+    for(int ii= 0 ; ii < dimension ; ++ ii)
+      res[ii]*=res[0];
 #endif
 #endif    
 }
@@ -217,6 +181,96 @@ inline std::string MixProblem<GridType,RangeProvider>
   std::string returnString = stream.str();
   return returnString;
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+//                      WRAPPERS FOR SPLIT OPERATOR                               //
+////////////////////////////////////////////////////////////////////////////////////
+
+template <class GridType, class RangeProvider>
+class NvStMixProblem:
+private MixProblem< GridType, RangeProvider>
+{
+  using BaseType=MixProblem< GridType, RangeProvider> ;
+  using BaseType::fixedTimeFunction;
+  constexpr static int  dimension = GridType::dimensionworld;
+  constexpr static int dimDomain = dimension;
+  constexpr static int dimRange=RangeProvider::rangeDim/2;
+  using FunctionSpaceType = typename Fem::FunctionSpace<typename GridType::ctype,
+                                                        double,
+                                                        GridType::dimensionworld,
+                                                        dimRange >;
+
+  using DomainFieldType = typename FunctionSpaceType :: DomainFieldType;
+  using DomainType      = typename FunctionSpaceType :: DomainType;
+  using RangeFieldType  = typename FunctionSpaceType :: RangeFieldType;
+  using RangeType       = typename FunctionSpaceType :: RangeType;
+
+  public:
+  NvStMixProblem():
+  BaseType()
+  {}
+
+  template< class DomainType , class RangeType>
+  void evaluate( const DomainType& arg, RangeType& res) const
+  {
+    evaluate(0,arg,res);
+  }
+
+  template< class DomainType , class RangeType>
+  void evaluate( const double time, const DomainType& arg, RangeType& res ) const
+  {
+    typename BaseType::RangeType resfull;
+    BaseType::evaluate( time , arg , resfull );
+    res[0]=resfull[0];
+    for( int ii = 0 ; ii<dimension ; ++ii)
+      res[1+ii]=resfull[1+ii];
+    res[dimension+1]=resfull[dimension+2];
+  }
+
+};
+
+template <class GridType, class RangeProvider>
+class AcMixProblem:
+private MixProblem< GridType, RangeProvider>
+{
+  using BaseType=MixProblem< GridType, RangeProvider> ;
+  using BaseType::fixedTimeFunction;
+  constexpr static int  dimension = GridType::dimensionworld;
+  constexpr static int dimDomain = dimension;
+  constexpr static int dimRange=RangeProvider::rangeDim/2;
+  using FunctionSpaceType = typename Fem::FunctionSpace<typename GridType::ctype,
+                                                        double,
+                                                        GridType::dimensionworld,
+                                                        dimRange >;
+
+  using DomainFieldType = typename FunctionSpaceType :: DomainFieldType;
+  using DomainType      = typename FunctionSpaceType :: DomainType;
+  using RangeFieldType  = typename FunctionSpaceType :: RangeFieldType;
+  using RangeType       = typename FunctionSpaceType :: RangeType;
+
+  public:
+  AcMixProblem():
+  BaseType()
+  {}
+
+  template< class DomainType , class RangeType>
+  void evaluate( const DomainType& arg, RangeType& res) const
+  {
+    evaluate(0,arg,res);
+  }
+
+  template< class DomainType , class RangeType>
+  void evaluate( const double time, const DomainType& arg, RangeType& res) const
+  {
+    typename BaseType::RangeType resfull;
+    BaseType::evaluate( time , arg , resfull );
+    res[0]=resfull[dimension+1];
+    res[1]=resfull[dimension+3];
+    for( int ii = 0 ; ii < dimension ; ++ii)
+     res[2+ii]=resfull[dimension+4+ii];
+  }
+
+};
 
 } // end namespace Dune
 #endif
