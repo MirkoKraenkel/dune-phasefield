5a791ed0cbf98d849bdf42b1600216dcf4cb9fed
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-05-13 09:50:03 +0200

    corrected penaltyfactor

--- dune/fem/operator/assembled/splitop/allencahntensor.hh
+++ dune/fem/operator/assembled/splitop/allencahntensor.hh
@@ -300,8 +300,9 @@ void PhasefieldAllenCahnTensor<Operator , AddFunction, Model, Flux,JacFlux >
 
   // compute penalty factor
   const double intersectionArea = normal.two_norm();
+  const double penaltyFactor = intersectionArea/std::min(areaEn_,areaNb_);
   const double localwidth = lastSpeed_*std::min(areaEn_,areaNb_)/intersectionArea;
-  const double penaltyFactor = 1./localwidth;
+
 
   const double weightInside=quadInside.weight( pt ); 
   const double weightOutside=quadOutside.weight( pt ); 
