8c331aeb9cd9c45a687bc86cfc8578a6c686ee51
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-06-09 21:03:43 +0200

    parameter renamed

--- src/problems/mixedscheme/sharpproblem.hh
+++ src/problems/mixedscheme/sharpproblem.hh
@@ -49,7 +49,7 @@ public:
     rhofactor_( Fem::Parameter::getValue<double> ("phasefield.rhofactor")),
     rho1_( Fem::Parameter::getValue<double> ("phasefield.mwp1")),
     rho2_( Fem::Parameter::getValue<double> ("phasefield.mwp2")),
-    phiscale_(Fem::Parameter::getValue<double> ("phiscale")),
+    phiscale_(Fem::Parameter::getValue<double> ("phasefield.phiscale")),
     radius_(Fem::Parameter::getValue<double>("phasefield.radius")),
     veloright_(Fem::Parameter::getValue<double>("phasefield.veloright")),
     veloleft_(Fem::Parameter::getValue<double>("phasefield.veloleft")),
