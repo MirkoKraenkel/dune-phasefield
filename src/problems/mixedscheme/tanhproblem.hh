b51ee739495a51d47f9ce3d103ee2154aa370524
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-08-27 15:27:46 +0200

    renamed the rho values, cosmetics

--- src/problems/mixedscheme/tanhproblem.hh
+++ src/problems/mixedscheme/tanhproblem.hh
@@ -8,6 +8,7 @@
 
 // local includes
 #include <dune/phasefield/modelling/thermodsurface.hh>
+//#include <dune/phasefield/modelling/thermodscaled.hh>
 
 #include <dune/fem-dg/models/defaultprobleminterfaces.hh>
 
@@ -42,8 +43,8 @@ public:
     delta_(Fem::Parameter::getValue<double>( "phasefield.delta" )),
     A_(Fem::Parameter::getValue<double>("phasefield.A")),
     rhofactor_( Fem::Parameter::getValue<double> ("phasefield.rhofactor")),
-    rho1_( Fem::Parameter::getValue<double> ("phasefield.mwpliq")),
-    rho2_( Fem::Parameter::getValue<double> ("phasefield.mwpvap")),
+    rholiq_( Fem::Parameter::getValue<double> ("phasefield.mwpliq")),
+    rhovap_( Fem::Parameter::getValue<double> ("phasefield.mwpvap")),
     phiscale_(Fem::Parameter::getValue<double> ("phasefield.phiscale")),
     radius_(Fem::Parameter::getValue<double>("phasefield.radius")),
     shift_(Fem::Parameter::getValue<double>("phasefield.shift",0.)),
@@ -101,8 +102,8 @@ public:
   const double delta_;
   double A_;
   double rhofactor_; 
-  double rho1_;
-  double rho2_;
+  double rholiq_;
+  double rhovap_;
   const double phiscale_;
   const double radius_;
   const double shift_;
@@ -136,11 +137,11 @@ inline void TanhProblem<GridType,RangeProvider>
 :: evaluate( const double t, const DomainType& arg, RangeType& res ) const 
 {
 
-  double rhodiff=rho2_-rho1_;
-  const double rhomean=1;//0.5*(rho2_+rho1_);
+  double rhodiff=mwpliq(0.)-mwpvap(0.);
+  const double rhomean=1;//0.5*(rhovap_+rholiq_);
 
 #if SURFACE
-  double deltaInv=rhomean/(delta_*phiscale_);
+  double deltaInv=1./delta_;
 #else  
   double deltaInv=sqrt(A_)/(delta_*phiscale_);
 #endif  
@@ -156,30 +157,27 @@ inline void TanhProblem<GridType,RangeProvider>
   double drdrtanhr=tanhr*drtanhr;
   //-2/delta^2 *(tanh*(1-tanh^2)
   drdrtanhr*=-2*deltaInv;
-  double phi=-0.5*tanhr+0.5;
+  double phi=-0.5*phiscale_*tanhr+0.5;
 
   
   double rho=evalRho(phi);
- // double rho=(rhodiff)*(-0.5*tanhrho+0.5)+rho1_;
-  //double rho=evalRho(phi); 
-  double v=veloright_*cos(2*M_PI*arg[0]);//-veloleft_)*(peak)+veloleft_;
+  //double rho=(rhodiff)*(0.5*-tanhrho+0.5)+rhovap_;
+
   //rho
   res[0]= rho;
- 
-   for(int i=1;i<=dimension;i++)
-   {
-      res[i]=v;
-   }
-   if(dimension==2)
-     res[2]=0;
 
-  
-   res[dimension+1]=phi;
- 
+  for(int ii = 1 ; ii <= dimension ; ii++ )
+  {
+    res[ ii ]=veloright_*cos( 2*M_PI*arg[ ii ] );
+  }
+
+
+  res[dimension+1]=phi;
+
 #if MIXED
-  double dFdphi=0;// thermodyn_.reactionSource(rho,phi); 
-  double dFdrho=0;// thermodyn_.chemicalPotential(rho, 0);
-  double sigma=0.5*drtanhr;
+  double dFdphi= thermodyn_.reactionSource(rho,phi,phi);
+  double dFdrho= thermodyn_.chemicalPotential(rho,phi,rho);
+  double sigma=0.5*phiscale_*drtanhr;
   double laplacePhi=0.5*drdrtanhr;
   if(arg[0]<0.)
     {
@@ -198,9 +196,9 @@ inline void TanhProblem<GridType,RangeProvider>
   res[dimension+4]=sigma;
 #else
   //mu
-  res[dimension+2]=0;0.5*v*v+dFdrho;
+  res[dimension+2]=dFdrho;
   //tau
-  res[dimension+3]=0;dFdphi-delta_*laplacePhi;
+  res[dimension+3]=dFdphi-delta_*A_*laplacePhi;
   //sigma_x
   res[dimension+4]=sigma;
 #endif
