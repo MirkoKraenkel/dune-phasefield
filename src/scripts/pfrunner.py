b025591de90640879523dc9144a3a93140759436
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-08-27 15:37:42 +0200

    allow to import paramter.log files

--- src/scripts/pfrunner.py
+++ src/scripts/pfrunner.py
@@ -21,12 +21,11 @@ class PhasefieldRunner:
     entryframe.pack()
     messageframe=Frame(master)
     messageframe.pack(side=BOTTOM)
-    
-    self.params=[ 'phasefield.delta','phasefield.mu1','phasefield.mu2','phasefield.endTime', 'fixedTimeStep', 
-                  'fem.prefix','phasefield.startLevel','phasefield.reactionrate','phasefield.alpha', 
+    self.params=[ 'paramfile','phasefield.delta','phasefield.mu1','phasefield.mu2','phasefield.endTime', 'fixedTimeStep',
+                  'fem.prefix','phasefield.startLevel','phasefield.reactionrate','phasefield.alpha',
                   'phasefield.beta','phasefield.A', 'phasefield.rhofactor','phasefield.addvisc',
-                  'phasefield.muvisc','phasefield.acpenalty','phasefield.phiscale','phasefield.timestepfactor','phasefield.shift','phasefield.veloleft','phasefield.veloright']
-    self.defaults=[ '0.05','0.1','0.1','1','0','','0','1','0.','1.','1','2',0,0,1,1,0.1,0,0 ,0]
+                  'phasefield.muvisc','phasefield.acpenalty','phasefield.gravity','phasefield.timestepfactor','phasefield.shift','phasefield.veloleft','phasefield.veloright']
+    self.defaults=['*.log', '0.05','0.1','0.1','10','0','','0','1','0.','1','0.1','0',0,1,1,0.,1,0,0 ,0]
     
     self.paramEntries=self.makeform(paramframe,self.params)
     
@@ -41,16 +40,19 @@ class PhasefieldRunner:
     self.make=Button( entryframe, text="Run", command=self.runit)
     self.make.pack(side=RIGHT)
     self.scrvar=IntVar()
-    
+    self.rdchck=IntVar()
+    readcheck=Radiobutton(entryframe,text='read from file', variable=self.rdchck,value=1).pack()
+    readcheck=Radiobutton(entryframe,text='read from form', variable=self.rdchck,value=0).pack()
+
     scr=Radiobutton(entryframe,text='screen',variable=self.scrvar,value=1).pack() 
     scr=Radiobutton(entryframe,text='show',variable=self.scrvar,value=0).pack() 
   
-  #create form from params
+###create form from params#####################
   def makeform( self , root ,fields):
-    
+
     entries={}
     count=0
-    
+
     for field in fields:
       row=Frame(root)
       lab=Label(row,text=field)
@@ -63,15 +65,28 @@ class PhasefieldRunner:
       count+=1
     
     return entries
+##############################################
+
+  def composeFileString( self ):
+
+    myday=time.strftime("%d_%m")
+    idxs = self.listbox.curselection()
+    index=int(idxs[0])
+    p=self.programms[index]
+    outfile='./Data_'+myday+'/'+self.paramEntries['fem.prefix'].get()
+    paramstring=''
+    paramstring+='fem.prefix:'+outfile+'_'+p+' '
+    execstring ='./'+p+' '+paramstring+' paramfile:'+self.paramEntries['paramfile'].get()
+    return ( execstring , outfile )
   
   #compose string passed to the command line
-  def composeString( self ):
+  def composeFormString( self ):
     
     myday=time.strftime("%d_%m") 
     idxs = self.listbox.curselection()
     index=int(idxs[0])
     p=self.programms[index]
-    outfile='./Data_'+myday+'/'+self.paramEntries['fem.prefix'].get()
+    outfile='/disk1/Data_'+myday+'/'+self.paramEntries['fem.prefix'].get()
     paramstring=''
     
     for key in self.paramEntries:
@@ -89,11 +104,9 @@ class PhasefieldRunner:
     #execstring ='./'+p+' '+paramstring+'paramfile:parameter_gui &>'+outfile+p+'.out'
     execstring ='./'+p+' '+paramstring+'paramfile:../parameterFiles/parameter'
     #execstring ='./'+p+' '+paramstring+'paramfile:../parameterFiles/parameter &> '+outfile+p+ '.out'
+    #
 
     #execstring ='gdb --args '+p+' '+paramstring+'paramfile:parameter_gui '
- 
-
-
     return ( execstring , outfile )
   
   #call the programm with parameter string directly or in a screen session
@@ -116,6 +129,11 @@ class PhasefieldRunner:
   
   #run the programm
   def runit(self):
-    
-    self.runcall(self.composeString())
 
+    if self.rdchck.get()==1:
+
+        self.runcall(self.composeFileString())
+
+    else:
+
+        self.runcall(self.composeFormString())
