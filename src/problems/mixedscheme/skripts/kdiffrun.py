835fa4f8c53a3abf6d3a6e21300979eee8a3c876
kraenkel <kraenkel@mathematik.uni-freiburg.de>
2015-09-10 15:47:19 +0200

    removed more old stuff

--- src/problems/mixedscheme/skripts/kdiffrun.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-import subprocess,sys
-number=int(sys.argv[1])
-nb=str(number)
-#subprocess.call(['./phasefield scheme:test&> pmatrix'+nb+'.out'],shell=True)
-subprocess.call(['./phasefieldcoupling scheme:test&> pmatrix'+nb+'.out'],shell=True)
-subprocess.call(['./phasefieldfd scheme:test&> pfd'+nb+'.out'],shell=True)
-subprocess.call(['kdiff3 pmatrix'+nb+'.out pfd'+nb+'.out'],shell=True)
